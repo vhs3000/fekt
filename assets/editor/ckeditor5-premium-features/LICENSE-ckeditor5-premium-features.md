23cfe1f008869dacc205494b12c0a948f91a1a36
vojta <vojtech.petrik@centrum.cz>
2026-09-18 18:59:25 +0200

    ckeditor init, refactor

---
+++ assets/editor/ckeditor5-premium-features/LICENSE-ckeditor5-premium-features.md
@@ -0,0 +1,54 @@
+Software License Agreement
+==========================
+
+**CKEditor&nbsp;5 Premium features** (https://ckeditor.com/ckeditor-5/)<br>
+Copyright (c) 2003–2026, [CKSource Holding sp. z o.o.](https://cksource.com) All rights reserved.
+
+CKEditor&nbsp;5 Premium features is licensed under a commercial license and is protected by copyright law.
+For more information, see: [https://ckeditor.com/pricing](https://ckeditor.com/pricing).
+
+Sources of Intellectual Property Included in CKEditor&nbsp;5
+------------------------------------------------------------
+
+Where not otherwise indicated, all CKEditor&nbsp;5 content is authored by CKSource engineers and consists of CKSource-owned intellectual property.
+
+The following libraries are included in CKEditor&nbsp;5 under the [BSD-2-Clause license](https://opensource.org/licenses/BSD-2-Clause):
+
+* domhandler - Copyright (c) Felix Böhm.
+* domutils - Copyright (c) Felix Böhm.
+
+The following libraries are included in CKEditor&nbsp;5 under the [BSD-3-Clause license](https://opensource.org/licenses/BSD-3-Clause):
+
+* diff - Copyright (c) 2009-2015, Kevin Decker <kpdecker@gmail.com>.
+* protobufjs - Copyright (c) 2016, Daniel Wirtz  All rights reserved.
+
+The following libraries are included in CKEditor&nbsp;5 under the [MIT license](https://opensource.org/licenses/MIT):
+
+* @codemirror/autocomplete - Copyright (C) 2018-2021 by Marijn Haverbeke <marijn@haverbeke.berlin> and others.
+* @codemirror/commands - Copyright (C) 2018-2021 by Marijn Haverbeke <marijn@haverbeke.berlin> and others.
+* @codemirror/lang-html - Copyright (C) 2018-2021 by Marijn Haverbeke <marijn@haverbeke.berlin> and others.
+* @codemirror/lang-markdown - Copyright (C) 2018-2021 by Marijn Haverbeke <marijn@haverbeke.berlin> and others.
+* @codemirror/language - Copyright (C) 2018-2021 by Marijn Haverbeke <marijn@haverbeke.berlin> and others.
+* @codemirror/state - Copyright (C) 2018-2021 by Marijn Haverbeke <marijn@haverbeke.berlin> and others.
+* @codemirror/theme-one-dark - Copyright (C) 2018-2021 by Marijn Haverbeke <marijn@haverbeke.berlin> and others.
+* @codemirror/view - Copyright (C) 2018-2021 by Marijn Haverbeke <marijn@haverbeke.berlin> and others.
+* @types/luxon - Copyright (c) Microsoft Corporation.
+* @uploadcare/file-uploader - Copyright (c) 2025 Uploadcare (hello@uploadcare.com). All rights reserved.
+* @uploadcare/upload-client - Copyright (c) 2019 Uploadcare Inc.
+* dom-serializer - Copyright (c) 2014 The cheeriojs contributors.
+* es-toolkit - Copyright (c) 2024 Viva Republica, Inc.
+* eventsource-parser - Copyright (c) 2025 Espen Hovlandsdal <espen@hovlandsdal.com>.
+* htmlparser2 - Copyright 2010, 2011, Chris Winberry <chris@winberry.net>. All rights reserved.
+* luxon - Copyright 2019 JS Foundation and other contributors.
+* morphdom - Copyright (c) Patrick Steele-Idem <pnidem@gmail.com> (psteeleidem.com).
+* rehype-sanitize - Copyright (c) 2016 Titus Wormer <tituswormer@gmail.com>.
+* socket.io-client - Copyright (c) 2014-present Guillermo Rauch and Socket.IO contributors.
+* socket.io-parser - Copyright (c) 2014-present Guillermo Rauch and Socket.IO contributors.
+* specificity - Copyright (c) 2016 Keegan Street and others.
+* url-parse - Copyright (c) 2015 Unshift.io, Arnout Kazemier, the Contributors.
+* uuid - Copyright (c) 2010-2020 Robert Kieffer and other contributors.
+
+Trademarks
+----------
+
+**CKEditor** is a trademark of [CKSource Holding sp. z o.o.](https://cksource.com) All other brand and product names are trademarks, registered trademarks or service marks of their respective holders.
---
+++ js/initArticleEditor.js
@@ -0,0 +1,189 @@
+/**
+ * This configuration was generated using the CKEditor 5 Builder. You can modify it anytime using this link:
+ * https://builder.ckeditor.com/#installation/NoNgNARAzAdALDAjBScQHYQA4AMBWAJigE448tiQREt085FFioDEocsQc0duo4cBOKSjVMOFBADGAZxQ4wwRGAULlagLqQApgEMoURLoAmEDUA==
+ */
+
+import {
+  ClassicEditor,
+  Autosave,
+  Essentials,
+  Paragraph,
+  Autoformat,
+  TextTransformation,
+  ListProperties,
+  List,
+  AdjacentListsSupport,
+  TodoList,
+  Bold,
+  Italic,
+  Underline,
+  Strikethrough,
+  Code,
+  Subscript,
+  Superscript,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+  RemoveFormat,
+  Highlight,
+  GeneralHtmlSupport,
+} from "ckeditor5";
+
+import translations from "ckeditor5/translations/cs.js";
+
+const LICENSE_KEY =
+  "eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3OTA5ODU1OTksImp0aSI6IjkzNDk3ZjNhLWM5MTItNDliZC04ZGE1LTI2MjFmOGVhM2MxYiIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJyZW1vdmVGZWF0dXJlcyI6WyJBSSJdLCJ2YyI6ImZiZWZjMzYxIn0.CMJx0oDZlQrTgODFntK_WPbp0IHwrCt0DnrPjRWi0dsSv2R5h9173RQYn-pMLXQX5UwAQ1Vbjro7e2ZgOfb5Lg";
+
+const editorConfig = {
+  attachTo: document.querySelector("#editor"),
+  root: {
+    placeholder: "Type or paste your content here!",
+    initialData: "",
+  },
+  toolbar: {
+    items: [
+      "undo",
+      "redo",
+      "|",
+      "fontSize",
+      "fontFamily",
+      "fontColor",
+      "fontBackgroundColor",
+      "|",
+      "bold",
+      "italic",
+      "underline",
+      "strikethrough",
+      "subscript",
+      "superscript",
+      "code",
+      "removeFormat",
+      "|",
+      "highlight",
+      "|",
+      "bulletedList",
+      "numberedList",
+      "todoList",
+    ],
+    shouldNotGroupWhenFull: false,
+  },
+  plugins: [
+    AdjacentListsSupport,
+    Autoformat,
+    Autosave,
+    Bold,
+    Code,
+    Essentials,
+    FontBackgroundColor,
+    FontColor,
+    FontFamily,
+    FontSize,
+    GeneralHtmlSupport,
+    Highlight,
+    Italic,
+    List,
+    ListProperties,
+    Paragraph,
+    RemoveFormat,
+    Strikethrough,
+    Subscript,
+    Superscript,
+    TextTransformation,
+    TodoList,
+    Underline,
+  ],
+  licenseKey: LICENSE_KEY,
+  autosave: {
+    /* See: https://ckeditor.com/docs/ckeditor5/latest/features/autosave.html */
+  },
+  fontFamily: {
+    supportAllValues: true,
+  },
+  fontSize: {
+    options: [10, 12, 14, "default", 18, 20, 22],
+    supportAllValues: true,
+  },
+  htmlSupport: {
+    allow: [
+      {
+        name: /^.*$/,
+        styles: true,
+        attributes: true,
+        classes: true,
+      },
+    ],
+  },
+  language: "cs",
+  list: {
+    properties: {
+      styles: true,
+      startIndex: true,
+      reversed: true,
+    },
+  },
+  translations: [translations],
+};
+
+configUpdateAlert(editorConfig);
+
+ClassicEditor.create(editorConfig)
+  .then((editor) => {
+    const form = document.querySelector("#article-form");
+
+    form.addEventListener("submit", (event) => {
+      const content = editor
+        .getData()
+        .replace(/<[^>]*>/g, "")
+        .trim();
+
+      if (!content) {
+        event.preventDefault();
+        alert("Obsah článku je povinný.");
+      }
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+/**
+ * This function exists to remind you to update the config needed for premium features.
+ * The function can be safely removed. Make sure to also remove call to this function when doing so.
+ */
+function configUpdateAlert(config) {
+  if (configUpdateAlert.configUpdateAlertShown) {
+    return;
+  }
+
+  const isModifiedByUser = (currentValue, forbiddenValue) => {
+    if (currentValue === forbiddenValue) {
+      return false;
+    }
+
+    if (currentValue === undefined) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const valuesToUpdate = [];
+
+  configUpdateAlert.configUpdateAlertShown = true;
+
+  if (!isModifiedByUser(config.licenseKey, "<YOUR_LICENSE_KEY>")) {
+    valuesToUpdate.push("LICENSE_KEY");
+  }
+
+  if (valuesToUpdate.length) {
+    window.alert(
+      [
+        "Please update the following values in your editor config",
+        "to receive full access to Premium Features:",
+        "",
+        ...valuesToUpdate.map((value) => ` - ${value}`),
+      ].join("\n"),
+    );
+  }
+}
